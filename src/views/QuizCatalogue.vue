8197870d840ce83dcf67d5f36ee55d2f88bc366c
alexblazevski <alexblazevski@live.com.au>
2019-08-17 12:51:57 +1000

    Configured grid layout to hold cards

--- src/views/QuizCatalogue.vue
+++ src/views/QuizCatalogue.vue
@@ -1,65 +1,85 @@
 <template>
-    <v-card
-            class="mx-auto"
-            max-width="400"
-    >
-        <v-system-bar color="indigo darken-2"></v-system-bar>
 
 
-        <v-container
-                class="pa-2"
-                fluid
-        >
-            <v-row>
-                <v-col
-                        v-for="card in cards"
-                        :key="card.title"
-                        v-bind="{ [`xs${card.flex}`]: true }"
-                >
-                    <v-card>
-                        <v-img
-                                :src="card.src"
-                                class="white--text"
-                                height="200px"
-                                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                        >
-                        <v-card-title class="align-end fill-height" v-text="card.title"></v-card-title>
-                        </v-img>
+        <v-row>
+            <v-col>
+                <v-card class="mx-auto"
+                        max-width="90%">
+                    <v-toolbar flat>
+                        <v-btn icon>
+                            <v-icon>arrow_back</v-icon>
+                        </v-btn>
+                        <v-toolbar-title>Albums</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-btn icon>
+                            <v-icon>search</v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                    <v-subheader>May</v-subheader>
+                    <v-container fluid>
+                        <v-row>
+                            <v-col v-for="i in 6" :key="i" cols="4">
+                                <v-col
+                                        v-for="card in cards"
+                                        :key="card.title"
+                                        v-bind="{ [`xs${card.flex}`]: true }"
+                                >
+
+                                <v-card>
+                                    <v-img
+                                            :src="card.src"
+                                            class="white--text"
+                                            height="200px"
+                                            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                                    >
+                                        <v-card-title class="align-end fill-height" v-text="card.title"></v-card-title>
+                                    </v-img>
 
 
-                        <v-card-text>
+                                        <v-card-text>
                             <span class="text--primary">
+                                <span class="headline">Quiz Title</span><br>
         <span class="body-1">Quiz Description</span><br>
         <span class="caption"> Quiz Difficulty: </span>
       </span>
-                        </v-card-text>
+                                        </v-card-text>
 
-                        <v-card-actions>
-                            <v-btn
-                                    text
-                                    color="green"
-                            >
-                                Play
-                            </v-btn>
-                            <v-btn
-                                    text
-                                    color="orange"
-                            >
-                                Edit
-                            </v-btn>
-                            <v-btn
-                                    text
-                                    color="red"
-                            >
-                                Delete
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-container>
-    </v-card>
+                                        <v-card-actions>
+                                            <v-btn
+                                                    text
+                                                    color="green"
+                                            >
+                                                Play
+                                            </v-btn>
+                                            <v-btn
+                                                    text
+                                                    color="orange"
+                                            >
+                                                Edit
+                                            </v-btn>
+                                            <v-btn
+                                                    text
+                                                    color="red"
+                                            >
+                                                Delete
+                                            </v-btn>
+                                        </v-card-actions>
+
+                                </v-card>
+                            </v-col>
+
+
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <v-footer class="mt-12"></v-footer>
+                </v-card>
+            </v-col>
+        </v-row>
+
 </template>
+
+
 
 <script>
     export default {
