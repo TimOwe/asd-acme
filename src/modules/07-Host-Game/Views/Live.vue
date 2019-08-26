74dcf4e32aa17c8e08bbbbfd965b153b942dbf56
hanirifai <hanrifai@publicisgroupe.net>
2019-08-26 15:04:10 +1000

    fix merge

--- src/modules/07-Host-Game/Views/Live.vue
+++ src/modules/07-Host-Game/Views/Live.vue
@@ -5,7 +5,7 @@
             <v-sheet color="blue" height="50" elevation="7" class="mt-5">
             <v-layout justify-center align-center fill-height class="pl-4 pr-4 text--white">
                 <v-flex xs4>
-                    <v-layout>Max People: {{currentSession.max_ppl}}</v-layout>
+                    <v-layout style="color:white;">Max People: {{currentSession.max_ppl}}</v-layout>
                 </v-flex>
                 <v-flex xs4>
                     <v-layout>Time Per Question: {{currentSession.max_time_pq}} sec</v-layout>
