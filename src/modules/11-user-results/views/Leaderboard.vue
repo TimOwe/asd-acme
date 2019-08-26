b649f96882cfccf20cf420c931b399ef3b87845a
thaiceee <thaimoocao@gmail.com>
2019-08-26 15:00:42 +1000

    added sorting by score

--- src/modules/11-user-results/views/Leaderboard.vue
+++ src/modules/11-user-results/views/Leaderboard.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
+        <v-layout justify-center class="headline pt-5">Welcome to the Leaderboard</v-layout>
+        <v-layout justify-center class="pt-9 pb-5" >
+            <v-btn  color="primary" @click="sortResults('asc')"><v-icon>mdi-chevron-up</v-icon>Sort Ascending</v-btn>
+            <v-btn @click="sortResults('desc')"><v-icon>mdi-chevron-down</v-icon> Sort Descending</v-btn>
+        </v-layout>
     <v-simple-table height="500px">
         <thead>
         <tr>
             <th class="text-left">Rank #</th>
             <th class="text-left">User ID</th>
+            <th class="text-left">User Name</th>
             <th class="text-left">Score</th>
             <th class="text-left">Time (mins)</th>
         </tr>
         </thead>
         <tbody>
         <!-- display sortedresults array in table -->
-        <tr v-for="(result,index) in sortedResults">
-            <td>{{index + 1}}</td>
+        <tr v-for="result in sortedResults">
+            <td>{{ result.rank }}</td>
             <td>{{ result.user_key }}</td>
+            <td>{{ result.calculatedName }}</td>
             <td>{{ result.score * 100 }}%</td>
             <td>{{new Date(result.timeend - result.timestart).getSeconds()}}</td>
         </tr>
@@ -46,6 +53,9 @@
                     });
                     // sort results by ascending order of score
                     this.sortedResults = results.sort((a,b) => b.score - a.score)
+                    for(var i=0; i<this.sortedResults.length;i++){
+                        this.sortedResults[i].rank = i+1;
+                    }
                 });
             },
             // function to push dummy data to firebase
@@ -53,8 +63,10 @@
                 this.testData.forEach(entry => {
                     this.$db.ref('/Leaderboard').push(entry);
                 });
+            },
+            sortResults(type){
+                return type === 'asc' ? this.sortedResults.sort((a,b) => b.score-a.score) : this.sortedResults.sort((a,b) => a.score-b.score);
             }
-
         },
         // dummy data
         data(){
@@ -62,6 +74,7 @@
                 testData: [{
                     key: '-L376r782kba',
                     user_key: '-L783tgydiuab',
+                    calculatedName: 'Alex Blazevski',
                     quiz_key: '111',
                     score: 0.49,
                     timestart: 1566105295428,
@@ -70,6 +83,7 @@
                     {
                         key: '-L3790uji9kba',
                         user_key: '-L783hbsbkfuab',
+                        calculatedName: 'Daniel Hartshorne',
                         quiz_key: '111',
                         score: 0.87,
                         timestart: 1566105295428,
@@ -78,6 +92,7 @@
                     {
                         key: '-L377894gh9a',
                         quiz_key: '222',
+                        calculatedName: 'Tim Owe',
                         user_key: '-L783fhsiuab',
                         score: 0.76,
                         timestart: 1566105295428,
