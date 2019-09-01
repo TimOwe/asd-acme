37dd6d2ae0d7fb35bba4d3e4b31d7ce9fc235e68
hartshorne2 <49086586+hartshorne2@users.noreply.github.com>
2019-09-01 20:10:36 +1000

    Acceptance Test

---
+++ cypress/integration/01-user-account.js
@@ -0,0 +1,29 @@
+describe('Register', function () {
+    before(() => {
+        cy.visit('http://localhost:8080/login')
+    })
+    it('Create User', function () {
+        cy.get('button').contains('Register').click();
+        cy.get('[name="fname"]').type('dandandandan');
+        cy.get('[name="lname"]').type('harthartharthart');
+        cy.get('[name="email"]').type('dan@asdf.com');
+        cy.get('[name="pass"]').type('passpasspasspass');
+        cy.get('.v-select').click()
+        cy.get('.v-select-list').click();
+        cy.get('button').contains('Save').click();
+        cy.get('button').contains('Close').click();
+    })
+    it('Login', function () {
+        cy.get('[name="logemail"]').type('dan@asdf.com');
+        cy.get('[name="logpass"]').type('passpasspasspass');
+        cy.get('button').contains('Log-In').click();
+    })
+    it('Delete user', function () {
+        cy.wait(500);
+        cy.get('[name="settings"]').click();
+        cy.wait(500);
+        cy.get('button').contains('Delete Account').click();
+        cy.url().should('eq', 'http://localhost:8080/')
+    })
+
+})
