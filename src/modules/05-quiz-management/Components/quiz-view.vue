7616107e353110720c97289aae2db351d62d731f
alexblazevski <alexblazevski@live.com.au>
2019-09-05 01:16:59 +1000

    Implemented Question Viewer Component

--- src/modules/05-quiz-management/Components/quiz-view.vue
+++ src/modules/05-quiz-management/Components/quiz-view.vue
@@ -17,17 +17,43 @@
 
             <v-card-text>
                 <span class="text--primary">
-                    <span class="body-1">{{description}}</span><br>
-                    <span class="caption">Created by: {{owner}}</span><br>
+                    <span class="subtitle-1">{{description}}</span><br>
+                    <span class="caption"><b>Created by: </b>{{owner}}</span><br>
                     <span class="caption">Questions: {{questions.length}}</span><br>
                 </span>
             </v-card-text>
 
-            <v-card v-for="(question,index) in questions" :key="question.id">
-                <v-layout justify-center class="headline">Question {{index+1}}</v-layout>
-                <v-layout justify-center class="subtitle-1">{{question.q}}</v-layout>
+            <v-expansion-panels>
+                <v-expansion-panel>
+                    <v-expansion-panel-header class="headline">Questions</v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <div class="d-flex">
+                            <v-checkbox v-model="readonly" label="Show Answers" align="end"></v-checkbox>
+                        </div>
+                        <v-list>
+                            <v-list-group v-for="question in questions" :key="question.q" no-action link>
+                                <template v-slot:activator>
+                                        <v-list-item-title class="title">{{key+1}}. {{question.q}}</v-list-item-title>
+                                        <v-list-item-action-text>{{question.score}} Points</v-list-item-action-text>
+                                </template>
 
-            </v-card>
+                                <div v-if="readonly===true">
+                                    <v-list-item-title class="body-1, pl-4">Answers</v-list-item-title>
+                                    <v-list-item>
+                                        <v-layout>
+                                            <v-flex xs2 v-for="(n,i) in 4" :key="n">
+                                                <v-container v-bind:class="{ 'green--text': answerCheck(question.c, i), 'black--text': answerCheck(question.c, i), 'body-1': true}" name="answer" v-model="question.a[i]" :label="'Answer #'+ n">{{n}}. {{question.a[i]}}</v-container>
+                                            </v-flex>
+                                        </v-layout>
+                                    </v-list-item>
+                                </div>
+                            </v-list-group>
+                        </v-list>
+
+                    </v-expansion-panel-content>
+                </v-expansion-panel>
+            </v-expansion-panels>
+
         </v-container>
 
         <v-dialog persistent scrollable fullscreen hide-overlay transition="dialog-bottom-transition" v-model="edit">
@@ -86,6 +112,9 @@
                 this.edit=false;
                 this.$emit('refresh');
             },
+            answerCheck: function(c, a){
+                return parseInt(c)===parseInt(a);
+            },
             onBackButton() {
                 this.$emit("catalogueView");
             },
@@ -98,13 +127,108 @@
             quizTitle: '',
             description: '',
             owner: '',
-            questions: '',
+            questions: [],
             img: '',
-            key: ''
+            key: '',
+            readonly: false,
+
+
+            expanded: [],
+            headers: [
+                {   text: 'Question', align: 'left', sortable: false, value: 'q',},
+                { text: 'Score', value: 'score' },
+            ],
+            desserts: [
+                {
+                    name: 'Frozen Yogurt',
+                    calories: 159,
+                    fat: 6.0,
+                    carbs: 24,
+                    protein: 4.0,
+                    iron: '1%',
+                },
+                {
+                    name: 'Ice cream sandwich',
+                    calories: 237,
+                    fat: 9.0,
+                    carbs: 37,
+                    protein: 4.3,
+                    iron: '1%',
+                },
+                {
+                    name: 'Eclair',
+                    calories: 262,
+                    fat: 16.0,
+                    carbs: 23,
+                    protein: 6.0,
+                    iron: '7%',
+                },
+                {
+                    name: 'Cupcake',
+                    calories: 305,
+                    fat: 3.7,
+                    carbs: 67,
+                    protein: 4.3,
+                    iron: '8%',
+                },
+                {
+                    name: 'Gingerbread',
+                    calories: 356,
+                    fat: 16.0,
+                    carbs: 49,
+                    protein: 3.9,
+                    iron: '16%',
+                },
+                {
+                    name: 'Jelly bean',
+                    calories: 375,
+                    fat: 0.0,
+                    carbs: 94,
+                    protein: 0.0,
+                    iron: '0%',
+                },
+                {
+                    name: 'Lollipop',
+                    calories: 392,
+                    fat: 0.2,
+                    carbs: 98,
+                    protein: 0,
+                    iron: '2%',
+                },
+                {
+                    name: 'Honeycomb',
+                    calories: 408,
+                    fat: 3.2,
+                    carbs: 87,
+                    protein: 6.5,
+                    iron: '45%',
+                },
+                {
+                    name: 'Donut',
+                    calories: 452,
+                    fat: 25.0,
+                    carbs: 51,
+                    protein: 4.9,
+                    iron: '22%',
+                },
+                {
+                    name: 'KitKat',
+                    calories: 518,
+                    fat: 26.0,
+                    carbs: 65,
+                    protein: 7,
+                    iron: '6%',
+                },
+            ],
         }),
     }
 </script>
 
 <style scoped>
+    green{
+        color:green;
+    }
+
+
 
 </style>
