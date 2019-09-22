ea31fba05c918cb0ae60884d212ee94f1ba625c2
hartshorne2 <49086586+hartshorne2@users.noreply.github.com>
2019-09-22 13:39:14 +1000

    Profile made

--- src/modules/01-User-Accounts/Views/Profile.vue
+++ src/modules/01-User-Accounts/Views/Profile.vue
@@ -12,7 +12,79 @@
                 <v-divider></v-divider>
                     <v-container fluid style="margin-left: 20px">
                         <v-row>
-                            <v-card-title style="color: royalblue; font-size: 25px">{{user.fname}} {{user.lname}}'s profile</v-card-title>
+                            <v-card-title style="color: royalblue; font-size: 25px">{{user.fname}} {{user.lname}}'s Profile:</v-card-title>
+                        </v-row>
+                        <v-row>
+                            <v-col sm="4">
+                                <div v-if="user.picture !== ''">
+                                    <v-img v-bind:src=user.picture></v-img>
+                                </div>
+                                <div v-else>
+                                    <v-img src="https://www.watsonmartin.com/wp-content/uploads/2016/03/default-profile-picture.jpg"></v-img>
+                                </div>
+                            </v-col>
+                            <v-divider vertical class="mx-12" ></v-divider>
+                            <v-col>
+                                <v-row>
+                                    <v-col sm="5" style="margin-left: -15px;">
+                                        <v-card-text style="font-size: 25px; color: royalblue">First Name:</v-card-text>
+                                    </v-col>
+                                    <v-col style="margin-left:-8%">
+                                        <v-card-text style="font-size: 25px;" >{{user.fname}}</v-card-text>
+                                    </v-col>
+                                </v-row>
+                                <v-row style="margin-top: -25px">
+                                    <v-col sm="5" style="margin-left: -15px;">
+                                        <v-card-text style="font-size: 25px; color: royalblue">Last Name:</v-card-text>
+                                    </v-col>
+                                    <v-col style="margin-left:-8%">
+                                        <v-card-text style="font-size: 25px;">{{user.lname}}</v-card-text>
+                                    </v-col>
+                                </v-row>
+                                <v-row style="margin-top: -25px">
+                                    <v-col  sm="5" style="margin-left: -15px;">
+                                        <v-card-text style="font-size: 25px; color: royalblue">Email:</v-card-text>
+                                    </v-col>
+                                    <v-col style="margin-left:-8%">
+                                        <v-card-text style="font-size: 25px;">{{user.email}}</v-card-text>
+                                    </v-col>
+                                </v-row>
+                                <v-row style="margin-top: -25px">
+                                    <v-col sm="5" style="margin-left: -15px;">
+                                        <v-card-text style="font-size: 25px; color: royalblue">Games Played:</v-card-text>
+                                    </v-col>
+                                    <v-col style="margin-left:-8%">
+                                        <v-card-text style="font-size: 25px;">20</v-card-text>
+                                    </v-col>
+                                </v-row>
+                                <v-row style="margin-top: -25px">
+                                    <v-col sm="5" style="margin-left: -15px;">
+                                        <v-card-text style="font-size: 25px; color: royalblue">Questions Answered:</v-card-text>
+                                    </v-col>
+                                    <v-col style="margin-left:-8%">
+                                        <v-card-text style="font-size: 25px;">87</v-card-text>
+                                    </v-col>
+                                </v-row>
+                                <v-row style="margin-top: -25px">
+                                    <v-col sm="5" style="margin-left: -15px;">
+                                        <v-card-text style="font-size: 25px; color: Green">Correct Answers:</v-card-text>
+                                    </v-col>
+                                    <v-col style="margin-left:-8%">
+                                        <v-card-text style="font-size: 25px;">67</v-card-text>
+                                    </v-col>
+                                </v-row>
+                                <v-row style="margin-top: -25px">
+                                    <v-col sm="5" style="margin-left: -15px;">
+                                        <v-card-text style="font-size: 25px; color: red">Incorrect Answers:</v-card-text>
+                                    </v-col>
+                                    <v-col style="margin-left:-8%">
+                                        <v-card-text style="font-size: 25px;">20</v-card-text>
+                                    </v-col>
+                                </v-row>
+                                <v-row v-if="this.$cookies.isKey('user') && (this.$route.params.id === this.$cookies.get('user').key)">
+                                    <v-btn text style="margin-left: 82%"><v-icon>mdi-pencil</v-icon>Edit</v-btn>
+                                </v-row>
+                            </v-col>
                         </v-row>
                     </v-container>
                 <v-footer></v-footer>
@@ -36,7 +108,7 @@
         },
         data(){
             return{
-                user: []
+                user: [],
             }
         }
     }
