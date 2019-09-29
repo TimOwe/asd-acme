39ce966d0619bc6862803c345e0c5b2240049852
hartshorne2 <49086586+hartshorne2@users.noreply.github.com>
2019-09-29 12:41:15 +1000

    Update 03.spec.js

--- tests/unit/03.spec.js
+++ tests/unit/03.spec.js
@@ -1,3 +1,16 @@
-//import { createLocalVue, shallowMount } from "@vue/test-utils";
-//import PasswordCard from "../../src/modules/03-user-administration/views/Users";
+  
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import PasswordCard from "../../src/modules/01-User-Accounts/components/Change-Password-Card";
 
+describe("Change Password", () => {
+    it("Password do not equal", () => {
+        const localVue = createLocalVue();
+        const wrapper = shallowMount(PasswordCard, {
+            localVue
+        });
+        wrapper.vm.pass = "password";
+        wrapper.vm.rpass = "noteuqal";
+        expect(wrapper.vm.isEqual()).toBe(false);
+        expect(wrapper.vm.validation).toEqual("Passwords do not match")
+    });
+});
