3c388d8de654b758901b6b50169c60d58bacb62e
alexblazevski <alexblazevski@live.com.au>
2019-10-12 19:08:05 +1100

    Additions of Category to tests

--- tests/unit/05.spec.js
+++ tests/unit/05.spec.js
@@ -18,6 +18,7 @@ describe("Creator Card", () => {
         wrapper.vm.quizTitle = "Unit Test";
         wrapper.vm.description = "This is a quiz for a unit test";
         wrapper.vm.img = { name: 'Colourful', url: 'https://imgur.com/zmxFPdu.jpg' };
+        wrapper.vm.category = { category: 'General Knowledge', value: 'generalknowledge' };
         wrapper.vm.questionBank = [{q: "Unit Test question 1", a: ["Unit Test Answer 1", "Unit Test Answer 2", "Unit Test Answer 3", "Unit Test Answer 4"],c: 0, score: 1000}];
         expect(wrapper.vm.validCheck()).toBe(true);
     });
@@ -34,6 +35,7 @@ describe("Creator Card", () => {
         wrapper.vm.quizTitle = "Unit Test";
         wrapper.vm.description = "This is a quiz for a unit test";
         wrapper.vm.img = { name: 'Colourful', url: 'https://imgur.com/zmxFPdu.jpg' };
+        wrapper.vm.category = { category: 'General Knowledge', value: 'generalknowledge' };
         wrapper.vm.questionBank = [{q: "", a: ["Unit Test Answer 1", "Unit Test Answer 2", "Unit Test Answer 3", "Unit Test Answer 4"],c: 0, score: 1000}];
         expect(wrapper.vm.validCheck()).toBe(false);
     });
